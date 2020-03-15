16a19c4f1f349c6b2abfa29cf4628c93bc36b37d
Ragnar Rebase <rrebase@gmail.com>
2020-03-15 13:06:13 +0200

    Add BoardDetailSerializer.

--- backend/boards/api.py
+++ backend/boards/api.py
@@ -2,13 +2,23 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from boards.models import Board, Task
-from boards.serializers import BoardSerializer, TaskSerializer
+from boards.serializers import BoardSerializer, TaskSerializer, BoardDetailSerializer
 
 
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return BoardDetailSerializer
+        return super().get_serializer_class()
+
+    def get_queryset(self):
+        if self.action == 'retrieve':
+            return super().get_queryset().prefetch_related('columns__tasks')
+        return super().get_queryset()
 
 
 class TaskViewSet(viewsets.ModelViewSet):
--- backend/boards/serializers.py
+++ backend/boards/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
 
-from boards.models import Board, Task
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ["id", "title", "column"]
+from boards.models import Board, Task, Column
 
 
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = ["id", "name"]
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ["id", "title", "description"]
+
+
+class ColumnSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Column
+        fields = ["id", "title", "tasks"]
+
+
+class BoardDetailSerializer(serializers.ModelSerializer):
+    columns = ColumnSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Board
+        fields = ["id", "name", "columns"]
