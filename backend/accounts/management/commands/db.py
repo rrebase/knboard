d6a093bface9389d47e4c00fdb3e9c88e98a9928
Ragnar Rebase <rrebase@gmail.com>
2020-03-27 02:10:37 +0200

    Add owner, member to fixtures and shortcut management commands.

---
+++ backend/accounts/management/commands/db.py
@@ -0,0 +1,14 @@
+from django.core import management
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = "Helpful command to run both makemigrations and migrate in one go"
+
+    def handle(self, *args, **options):
+        if not settings.DEBUG:
+            raise CommandError("Command not meant for production")
+
+        management.call_command("makemigrations")
+        management.call_command("migrate")
---
+++ backend/accounts/management/commands/load.py
@@ -0,0 +1,14 @@
+from django.core import management
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = "Helpful command to load all fixtures"
+
+    def handle(self, *args, **options):
+        if not settings.DEBUG:
+            raise CommandError("Command not meant for production")
+
+        management.call_command("loaddata", "users")
+        management.call_command("loaddata", "tasks")
