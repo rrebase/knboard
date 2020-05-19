1f4fed171038ae6fdeb93cb2cd251f4fbdb7111e
Ragnar Rebase <rrebase@gmail.com>
2020-05-19 23:12:59 +0300

    Move redirect into http block.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(knboard.com www.knboard.com)
+domains=(knboard.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rebasedev@gmail.com" # Adding a valid address is strongly recommended
