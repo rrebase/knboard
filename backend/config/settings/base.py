f417dfb30613bc6f788b0ac2257adf777e9d916b
Ragnar Rebase <rrebase@gmail.com>
2020-05-24 19:55:47 +0300

    Update django static url to match new nginx location conf.

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -112,8 +112,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = "/static/"
-STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/django-static/")
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./django-static/")
 
 # Media files
 MEDIA_ROOT = "./media"
--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -4,6 +4,7 @@ from ..env_utils import get_env
 
 DEBUG = False
 SECRET_KEY = get_env("DJANGO_SECRET_KEY")
+STATIC_URL = get_env("DJANGO_STATIC_URL")
 STATIC_ROOT = get_env("DJANGO_STATIC_ROOT")
 ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS").split(",")
 
