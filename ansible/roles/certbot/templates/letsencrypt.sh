8349bf79e73e6cd57c1304314f2e49cb2cd1c47d
Ragnar Rebase <rrebase@gmail.com>
2020-06-28 10:52:48 +0300

    Use variables in letsencrypt template.

--- ansible/roles/certbot/templates/letsencrypt.sh
+++ ansible/roles/certbot/templates/letsencrypt.sh
@@ -6,10 +6,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(thatapp.xyz)
+domains=({{ domain_name }})
 rsa_key_size=4096
 data_path="./data/certbot"
-email="rrebase@gmail.com" # Adding a valid address is strongly recommended
+email="{{ letsencrypt_email }}" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
