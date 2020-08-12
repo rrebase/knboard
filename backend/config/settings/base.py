bbbdd2401514cf808da0bc9db077f857eb63cbe1
Arvand <arvand.fazeli@gmail.com>
2020-08-12 08:30:37 -0700

    add rest filter

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -132,6 +132,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
+    "DEFAULT_FILTER_BACKENDS": ("rest_framework_filters.backends.DjangoFilterBackend",),
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
 
