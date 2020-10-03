e94f8615ea25ff3f36b6648bf4e50e1c95aa6135
Mark Doyle <mark@markdoyle.dev>
2020-10-04 00:14:09 +0100

    bug: update changes to tests

--- frontend/cypress/integration/stubbed/task.spec.js
+++ frontend/cypress/integration/stubbed/task.spec.js
@@ -25,7 +25,7 @@ context("Task", () => {
     const title = "Improve CI";
     cy.route("POST", "api/tasks/", { ...createTaskResponse, title });
 
-    cy.findAllByText("Add another card (⌘ + 2)").first().click();
+    cy.findAllByText("Add another card (ctrl + 2)").first().click();
     cy.findByTestId("create-task-title").type(title);
     cy.findByTestId("task-create").click();
     cy.findByText(title).should("be.visible");
