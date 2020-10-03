021cdb930b5eaa778470c74f6e32b40226b750c4
Krisan Alifari <akrifari@gmail.com>
2020-10-04 06:47:09 +0700

    Test against document.title

--- frontend/cypress/integration/stubbed/profile.spec.js
+++ frontend/cypress/integration/stubbed/profile.spec.js
@@ -6,6 +6,7 @@ context("Profile", () => {
     cy.route("GET", "/api/users/1/", "fixture:testuser.json");
     cy.route("/api/avatars/", "fixture:avatars.json");
     cy.visit("/profile/");
+    cy.title().should("eq", "Profile | Knboard");
   });
 
   it("should change username", () => {
