1b8088932d0882fce8caa549f5d9bcc0af3e9d6f
Mirjam Pajumägi <mirpaj@ttu.ee>
2020-10-13 18:00:37 +0300

    Impmlement FE test for requiring email when registering (issue #39)

--- frontend/cypress/integration/stubbed/auth.spec.js
+++ frontend/cypress/integration/stubbed/auth.spec.js
@@ -13,3 +13,26 @@ context("Stubbed Auth", () => {
     cy.findByTestId("open-login-btn").should("be.visible");
   });
 });
+
+context("Stubbed Auth User Not Logged In", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.title().should("eq", "Knboard");
+  });
+
+  it("should not register if no email entered", () => {
+    cy.findByText(/register/i).click();
+    cy.findByLabelText("Username").type("testuser");
+    cy.get("p[id='email-helper-text']").should(
+      "not.have.text",
+      "Can be left empty."
+    );
+    cy.findByLabelText("Password").type("TestPw123");
+    cy.findByLabelText("Confirm Password").type("TestPw123");
+    cy.findByTestId("submit-register-btn").click();
+    cy.wait(100);
+    cy.get("p[id='email-helper-text']")
+      .should("be.visible")
+      .should("have.text", "This field is required.");
+  });
+});
