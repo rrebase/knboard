bcfb9bbebfcaf90a670fafc954a566acfd59fadc
Mirjam Pajumägi <mirpaj@ttu.ee>
2020-10-26 11:22:10 +0200

    Mock server response (issue #39)

--- frontend/cypress/integration/stubbed/auth.spec.js
+++ frontend/cypress/integration/stubbed/auth.spec.js
@@ -16,20 +16,31 @@ context("Stubbed Auth", () => {
 
 context("Stubbed Auth User Not Logged In", () => {
   beforeEach(() => {
+    cy.server({ force404: true });
     cy.visit("/");
     cy.title().should("eq", "Knboard");
   });
 
   it("should not register if no email entered", () => {
+    cy.route({
+      method: "POST",
+      url: "/auth/registration/",
+      status: 400,
+      response: { email: ["This field is required."] },
+    }).as("register");
     cy.findByText(/register/i).click();
     cy.findByLabelText("Username").type("testuser");
-    cy.get("p[id='email-helper-text']").should("not.exist");
+    cy.get("p[id='email-helper-text']").should(
+      "not.have.text",
+      "Can be left empty."
+    );
     cy.findByLabelText("Password").type("TestPw123");
     cy.findByLabelText("Confirm Password").type("TestPw123");
     cy.findByTestId("submit-register-btn").click();
-    cy.wait(500);
-    cy.get("p[id='email-helper-text']")
-      .should("be.visible")
-      .should("have.text", "This field is required.");
+    cy.wait("@register").then(() => {
+      cy.get("p[id='email-helper-text']")
+        .should("be.visible")
+        .should("have.text", "This field is required.");
+    });
   });
 });
